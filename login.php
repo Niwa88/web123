6378dbfec73d2939e0ffe6712e899dd19ba56e64
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-11-12 15:04:57 +0700

    update login

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <form method="post">
 
-    <table style="margin: auto;">
+    <table>
         <tr>
             <td>NIP</td>
             <td><input name="nip" placeholder="Masukan NIP"></td>
